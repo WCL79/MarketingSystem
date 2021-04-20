5ad2100d5a0c36e8364bb9bfef94d334933d4f17
Weslley Lima <weslley.lima@zup.com.br>
2021-04-20 15:17:17 -0300

    Ajustar relação entre as entidades Produto com a Categoria

--- src/main/java/br/com/zup/model/Produto.java
+++ src/main/java/br/com/zup/model/Produto.java
@@ -3,10 +3,15 @@ package br.com.zup.model;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import java.util.List;
 
 public class Produto {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String nome;
+
+    @ManyToMany
+    private List<Categoria> categorias;
 }
