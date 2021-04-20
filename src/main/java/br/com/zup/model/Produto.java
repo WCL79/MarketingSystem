bd91abc69a142f362fef91d9fe23a58346d65f61
Weslley Lima <weslley.lima@zup.com.br>
2021-04-20 15:43:12 -0300

    Ajustar todas colunas de todas Entidades

--- src/main/java/br/com/zup/model/Produto.java
+++ src/main/java/br/com/zup/model/Produto.java
@@ -1,12 +1,9 @@
 package br.com.zup.model;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 public class Produto {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String nome;
 }
