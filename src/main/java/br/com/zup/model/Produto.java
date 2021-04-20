8c17fe31c99e0d060ba52f86e0e40fb37aaa9fc3
Weslley Lima <weslley.lima@zup.com.br>
2021-04-20 16:43:28 -0300

    Ajustar todas colunas de todas Entidades

--- src/main/java/br/com/zup/model/Produto.java
+++ src/main/java/br/com/zup/model/Produto.java
@@ -1,9 +1,28 @@
 package br.com.zup.model;
 
-import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@AllArgsConstructor//Construtor com todos argumentos
+@Entity
+@Table(name = "produtos")
 public class Produto {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(length = 155)
     private String nome;
+
+    @ManyToMany
+    private List<Categoria> categorias;
+
+    public Produto() {
+    }
 }
