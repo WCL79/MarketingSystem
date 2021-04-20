cb272526cf73cd9b7411f1a150ac0e9ee3248b0a
Weslley Lima <weslley.lima@zup.com.br>
2021-04-20 14:30:16 -0300

    Criar estrutura do projeto e configurações

---
+++ src/main/java/br/com/zup/SistemaMarketingApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaMarketingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaMarketingApplication.class, args);
+	}
+
+}
